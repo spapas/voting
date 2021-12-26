a53a669eff83bc98cc9782c435208addc871cdf1
Serafeim Papastefanos <spapas@gmail.com>
2021-12-26 22:42:08 +0200

    Initial

---
+++ contracts/Voter.sol
@@ -0,0 +1,77 @@
+//SPDX-License-Identifier: Unlicense
+pragma solidity ^0.8.0;
+
+import "@openzeppelin/contracts/access/Ownable.sol";
+import "hardhat/console.sol";
+
+contract Voter is Ownable {
+    address public voteFrom; 
+    string public question;
+    string public choice_a;
+    string public choice_b;
+    string public choice_c;
+    uint public vote_a;
+    uint public vote_b;
+    uint public vote_c;
+    uint public finishTime;
+    uint dayTime = 1 days;
+    uint dayFee = 0.01 ether;
+
+    event StartVote(address indexed startVoteFrom, string question, string choice_a, string choice_b, string choice_c, uint finishTime);
+    event Vote(address indexed voteFrom, uint indexed choice, uint total);
+
+    constructor() {
+        finishTime = block.timestamp;
+    }
+
+    modifier voteFinished() {
+        require(block.timestamp >= finishTime, "Vote is not finished yet!");
+        _;
+    }
+
+    modifier voteNotFinished() {
+        require(block.timestamp < finishTime, "Vote is finished!");
+        _;
+    }
+
+    function withdraw() external onlyOwner {
+        address _owner = owner();
+        payable(_owner).transfer(address(this).balance);
+    }
+
+    function startVote(string calldata _question, string calldata _choice_a, string calldata  _choice_b, string calldata  _choice_c, uint _days) public payable voteFinished {
+        require(msg.value == dayFee * _days, "Invalid fee!");
+        question = _question;
+        choice_a = _choice_a;
+        choice_b = _choice_b;
+        choice_c = _choice_c;
+        finishTime = block.timestamp + _days * dayTime;
+        voteFrom = msg.sender;
+        emit StartVote(msg.sender, question, choice_a, choice_b, choice_c, finishTime);
+    }
+
+    function vote(uint _choice) public voteNotFinished {
+        
+        uint tot = 0;
+        if (_choice == 0) {
+            vote_a += 1;
+            tot = vote_a;
+        } else if (_choice == 1) {
+            vote_b += 1;
+            tot = vote_b;
+        } else {
+            vote_c += 1;
+            tot = vote_c;
+        }
+        emit Vote(msg.sender, _choice, tot);
+    }
+
+    function getResult() public view returns (uint, uint, uint) {
+        return (vote_a, vote_b, vote_c);
+    }
+
+    function getVoteInfo() public view returns (string memory, string memory, string memory, string memory, uint) {
+        return (question, choice_a, choice_b, choice_c, finishTime);
+    }
+    
+}
---
+++ test/voter-test.js
@@ -0,0 +1,112 @@
+const { expect } = require("chai");
+const { ethers, waffle } = require("hardhat");
+
+let Voter;
+let voter
+let owner;
+
+beforeEach(async function () {
+  Voter = await ethers.getContractFactory("Voter");
+  voter = await Voter.deploy();
+  [owner] = await ethers.getSigners();
+  await voter.deployed();
+});
+
+describe("Voter", function () {
+  it("Should return a non usable vote when constructed", async function () {
+    
+    const finishTime = await voter.finishTime();
+    const latestBlock = await hre.ethers.provider.getBlock("latest")
+    expect(finishTime).to.equal(latestBlock.timestamp);
+
+  });
+
+  it("Should need the correct fee", async function () {
+    await expect(voter.startVote("q", "a1", "a2", "a3", 5, {
+      value: ethers.utils.parseEther("0.04")
+    })).to.be.revertedWith("Invalid fee!");
+  });
+
+  it("Should not allow anther vote", async function () {
+
+    await voter.startVote("q", "a1", "a2", "a3", 5, {
+      value: ethers.utils.parseEther("0.05")
+    });
+
+    await expect(voter.startVote("q", "a1", "a2", "a3", 5, {
+      value: ethers.utils.parseEther("0.05")
+    })).to.be.revertedWith("Vote is not finished yet!");
+  });
+
+  it("Should start a vote with correct data", async function () {
+    await voter.startVote("q", "a1", "a2", "a3", 5, {
+      value: ethers.utils.parseEther("0.05")
+    });
+    const latestBlock = await hre.ethers.provider.getBlock("latest")
+    const [a,b,c] = await voter.getResult();
+    expect(a).to.equal(0);
+    expect(b).to.equal(0);
+    expect(c).to.equal(0);
+    const [q, a1, a2, a3, ft] = await voter.getVoteInfo();
+    expect(q).to.equal("q");
+    expect(a1).to.equal("a1");
+    expect(a2).to.equal("a2");
+    expect(a3).to.equal("a3");
+    expect(ft).to.equal(latestBlock.timestamp+86400*5);
+  });
+
+  it("Should emit event with correct data data", async function () {
+    const latestBlock = await hre.ethers.provider.getBlock("latest")
+    await expect(voter.startVote("q", "a1", "a2", "a3", 5, {
+      value: ethers.utils.parseEther("0.05")
+    })).to.emit(voter, 'StartVote');//.withArgs(owner.address, "q", "a1", "a2", "a3", latestBlock.timestamp+86400*5);
+  });
+
+  it("Should do the vote", async function () {
+    await voter.startVote("q", "a1", "a2", "a3", 5, {
+      value: ethers.utils.parseEther("0.05")
+    });
+    await expect(voter.vote(0)).to.emit(voter, 'Vote').withArgs(owner.address, 0, 1);
+    await expect(voter.vote(0)).to.emit(voter, 'Vote').withArgs(owner.address, 0, 2);
+    await expect(voter.vote(2)).to.emit(voter, 'Vote').withArgs(owner.address, 2, 1);
+    const [a,b,c] = await voter.getResult();
+    expect(a).to.equal(2);
+    expect(b).to.equal(0);
+    expect(c).to.equal(1);
+  });
+
+  it("Should not allow voting on finished votes", async function () {
+    await expect(voter.vote(0)).to.be.revertedWith("Vote is finished!");
+  });
+
+  function r(v) {
+    return (Math.round(ethers.utils.formatEther(v)*100)/100);
+  }
+
+  it("Should withdraw funds", async function () {
+    const provider = waffle.provider;
+    
+    let ownerBalance = await provider.getBalance(owner.address);
+    let voterBalance = await provider.getBalance(voter.address);
+    expect(voterBalance).to.equal(ethers.utils.parseEther("0.0"));
+    await voter.startVote("q", "a1", "a2", "a3", 5, {
+      value: ethers.utils.parseEther("0.05")
+    });
+    voterBalance = await provider.getBalance(voter.address);
+    expect(voterBalance).to.equal(ethers.utils.parseEther("0.05"));
+    
+    let ownerBalanceAfterStart = await provider.getBalance(owner.address);
+    
+    await voter.withdraw();
+    let ownerBalanceAfterWithdraw = await provider.getBalance(owner.address);
+    //console.log(r(ownerBalance))
+    //console.log(r(ownerBalanceAfterStart))
+    //console.log(r(ownerBalanceAfterWithdraw))
+    expect(r(ownerBalance)).to.be.closeTo(r(ownerBalanceAfterStart) + 0.05, 0.0001);
+    expect(r(ownerBalance)).to.be.closeTo(r(ownerBalanceAfterWithdraw), 0.0001);
+    
+
+
+  });
+
+});
